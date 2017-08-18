05698205bba6daa16743b6f43ba1fead1562ae49
vladiko <koganvladimir@yahoo.com>
2017-08-18 12:13:10 +0300

    commit all initially not committed files

---
+++ routes/partaker.ts
@@ -0,0 +1,8 @@
+﻿import express = require('express');
+const router = express.Router();
+
+router.get('/', (req: express.Request, res: express.Response) => {
+    res.send("respond with a resource from partake dd r");
+});
+
+export default router;
